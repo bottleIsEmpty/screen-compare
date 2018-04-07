66a298981c83fe852fed1e5289d0492dd2639e87
Alexandr Bobko <sasha.bobcko@gmail.com>
2018-04-08 00:48:22 +0300

    fixed a bug

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const util = require('util');
 const jimp = require('jimp');
 const rmdir = require('rmdir');
 const natsort = require('natsort');
+const cmd = require('node-cmd');
 
 const VIDEOS_DIR = './videos';
 const CYCLE_DURATION = 10;
@@ -32,6 +33,7 @@ log4js.configure({
 const logger = log4js.getLogger();
 const deleteFile = util.promisify(fs.unlink);
 const readFile = util.promisify(fs.readFile);
+const readdir = util.promisify(fs.readdir);
 
 async function getDiff(img1, img2) {
 
@@ -54,8 +56,6 @@ async function prepareFiles() {
 
         logger.info('Preparing directory...');
 
-        const readdir = util.promisify(fs.readdir);
-        const deleteDir = util.promisify(rmdir);
         let files = await readdir(VIDEOS_DIR);
         let filteredFiles = [];
 
@@ -72,6 +72,9 @@ async function prepareFiles() {
                         logger.error(err);
                     }
                 });
+                continue;
+            } else if (file.search('.m3u') >= 0) {
+                await deleteFile(`${VIDEOS_DIR}/${file}`);
                 continue;
             }
 
@@ -101,7 +104,7 @@ async function readVideos() {
     try {
         logger.info(`Reading files from "${VIDEOS_DIR}"...`);
 
-        const files = await prepareFiles()
+        const files = await prepareFiles();
 
         return files;
     } catch (error) {
@@ -115,6 +118,8 @@ async function getFrames(video) {
         const process = await new ffmpeg(`${VIDEOS_DIR}/${video}`);
         const folderName = `${video}_temp`;
         const filepath = `${VIDEOS_DIR}/${folderName}`;
+        const cmdGet = util.promisify(cmd.get);
+        let framesFullName = [];
 
         logger.info(`Making screens of ${video}`);
 
@@ -122,13 +127,22 @@ async function getFrames(video) {
         fs.mkdirSync(filepath);
 
         // making screens with ffmpeg package
-        const frames = await process.fnExtractFrameToJPG(filepath, {
-            frame_rate: 1,
-            every_n_seconds: 10
-        });
+        // await process.fnExtractFrameToJPG(filepath, {
+        //     frame_rate: 1,
+        //     every_n_seconds: 10
+        // });
+        await cmdGet(`ffmpeg -i "${VIDEOS_DIR}/${video}" -vf fps=1/10 "${filepath}/%d.jpg"`);
+        
+        logger.debug('I AM HERE');
+
+        const frames = await readdir(filepath);
+
+        for (frame of frames) {
+            framesFullName.push(`${VIDEOS_DIR}/${folderName}/${frame}`);
+        }
 
         return {
-            frames: frames,
+            frames: framesFullName,
             directory: folderName
         };
 
@@ -193,12 +207,14 @@ async function cropAndMakePNG(frameData) {
 
         for (frame of frameData.frames) {
             try {
-                const filename = frame.slice(20, frame.length - 4);
+                let filename = frame.split('/')[3];
+                filename = filename.slice(0, filename.length - 4);
+
                 const img = await readFile(frame);
 
                 const newFrame = `${VIDEOS_DIR}/${frameData.directory}/${filename}.png`;
 
-                logger.debug(frameData.directory);
+                logger.debug('FRAME NAME:', filename, frameData.directory);
 
                 img.crop(85, 150, 200, 45);
                 img.write(newFrame);
@@ -226,25 +242,39 @@ async function generateM3U(timecodes, video, directory) {
 
         const writeFile = util.promisify(fs.writeFile);
         let data = '';
+        let videoName = video.slice(0, video.length - 4);
 
         timecodes.forEach((timecode, i) => {
             if (i === 0) {
                 data += `#EXTVLCOPT:start-time=${timecode}\n`;
             } else if (i < timecodes.length - 1) {
                 data += `#EXTVLCOPT:stop-time=${timecode}\n`;
-                data += `../${video}\n`;
+                data += `${video}\n`;
                 data += `#EXTVLCOPT:start-time=${timecode}\n`;
             } else {
                 data += `#EXTVLCOPT:stop-time=${timecode}\n`;
-                data += `../${video}\n`;
+                data += `${video}\n`;
             }
         });
 
-        await writeFile(`${VIDEOS_DIR}/${directory}/result.m3u`, data);
+        await rmdir(`${VIDEOS_DIR}/${directory}`);
 
-        logger.log(`~~FILE ${video} HANDLED SUCCESFULLY!~~`);
+        await writeFile(`${VIDEOS_DIR}/${videoName}.m3u`, data);
+
+        const result = `FILE ${video} HANDLED SUCCESFULLY! .M3U FILE GENERATED AT ${VIDEOS_DIR}/${directory}`;
+
+        logger.trace(result);
+
     } catch (err) {
         logger.error(err)
+    }
+}
+
+async function clearDir(directory) {
+    let files = await readdir(`${VIDEOS_DIR}/${directory}`);
+
+    for(file of files) {
+        await deleteFile(`${VIDEOS_DIR}/${directory}/${file}`);
     }
 }
 
@@ -262,5 +292,7 @@ async function generateM3U(timecodes, video, directory) {
 
         generateM3U(timecodes, video, directory);
     }
+
+    logger.info('FINISHED SUCCESFULLY! CHECK THE .M3U FILES!');
 
 })()
